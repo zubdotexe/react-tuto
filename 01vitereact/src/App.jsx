e5308604b1a7f1d700c4706e36fcbe2cbdc6d221
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2025-03-11 02:16:29 +0600

    understanding proj structure of vite

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,14 @@
+import Chai from './chai.jsx'
+
 function App() {
 
   return (
-    <h1>hello world using vite</h1>
+    // <h1>hello world using vite</h1>
+    <Chai/>
+
+    // jsx er belay ei function theke kebol ekta element return kora jay
+    // ekarone multiple elem return korte chaile <div> er moddhe return korte pari
+    // ei jinish ta eto common je react e <div> er bodole <></> use kora hoise pore ar etake fragment bola hocche
   )
 }
 
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,14 @@
+function Chai() {
+    return (
+        <h2>hello from Chai() using vite</h2>
+    )
+}
+
+export default Chai;
+
+// conventions 
+/*
+    1. component name PascalCase hobe
+    2. file name o PascalCase e hobe
+    3. file extension .jsx hobe
+*/
