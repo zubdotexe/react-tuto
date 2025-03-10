8a3a6d01f0cdf852c054d50c0a855c333afb15c5
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2025-03-11 02:49:04 +0600

    set attributes using loop instead of hard-coding

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -10,12 +10,25 @@ const reactElem = {
 const mainContainer = document.querySelector('#root');
 
 function customRender(reactElem, mainContainer) {
+    // const domElem = document.createElement(reactElem.type);
+    // domElem.innerHTML = reactElem.children;
+    // domElem.setAttribute('href', reactElem.props.href);
+    // domElem.setAttribute('target', reactElem.props.target);
+
+    // mainContainer.append(domElem);
+
+    // as the above lines are hard-coded thus, if the number of props increase or decrease, the code won't work, and that's y we will be using a loop
+
     const domElem = document.createElement(reactElem.type);
     domElem.innerHTML = reactElem.children;
-    domElem.setAttribute('href', reactElem.props.href);
-    domElem.setAttribute('target', reactElem.props.target);
 
-    mainContainer.append(domElem);
+    for(let prop in reactElem.props) {
+        if(prop === 'children') continue;
+
+        domElem.setAttribute(prop, reactElem.props[prop]);
+    }
+
+    mainContainer.appendChild(domElem);
 }
 
 customRender(reactElem, mainContainer)
