13ee9b344e134eb1d6dc0553cd919283a324e9b1
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2025-05-10 21:58:26 +0600

    initialized proj contextLocalStorage

---
+++ 10contextLocalStorage/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { TodoContextProvider } from "./contexts/todoContext";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [...prev, {id: Date.now(), ...todo}])
+  }
+
+  return (
+    <TodoContextProvider value={{todos, addTodo, editTodo, removeTodo, toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </TodoContextProvider>
+  );
+}
+
+export default App;
