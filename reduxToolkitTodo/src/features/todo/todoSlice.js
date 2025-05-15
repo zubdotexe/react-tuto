0dc9ba717250b03be751b433953b775b9a919834
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2025-05-15 21:09:11 +0600

    added todoSlice

---
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,33 @@
+import {createSlice, nanoid} from '@reduxjs/toolkit';
+
+const initialState = {
+    todos: [{
+        id: 1,
+        text: "hello world",
+    }]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            };
+            state.todos.push(todo);
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+        },
+        editTodo: (state, action) => {
+            const todo = state.todos.find(todo => todo.id === action.payload);
+            if (todo) todo.text = action.payload;
+        }
+    }
+})
+
+export const {addTodo, removeTodo, editTodo} = todoSlice.actions; // egular through te state update korbo
+
+export default todoSlice.reducer; // store je shokol reducer theke value niye nijeke update korbe, shei reducer gular list dite hobe store ke
